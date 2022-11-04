d5c975342aa56098babd1d9ff4122fdbe6b94d38
elle510 <elle0510@gmail.com>
2022-11-04 16:48:22 +0900

    ConfigModule을 이용한 .env 설정

--- back/src/app.service.ts
+++ back/src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly configService: ConfigService) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getEnv(): string {
+    // return process.env.SECRET;
+    // process 를 사용하지 않고 configService 를 이용하여 사용
+    return this.configService.get('SECRET');
   }
 }
