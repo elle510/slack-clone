8fd0bb4e5a96d7ee2ec1ed9fcfa6f0eac5f1cdac
elle510 <elle0510@gmail.com>
2022-11-24 16:14:50 +0900

    typeorm 설정

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,5 +1,7 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
@@ -7,6 +9,7 @@ import { UsersModule } from './users/users.module';
 import { WorkspacesModule } from './workspaces/workspaces.module';
 import { ChannelsModule } from './channels/channels.module';
 import { DmsModule } from './dms/dms.module';
+import { Users } from './entities/Users';
 
 @Module({
   imports: [
@@ -15,6 +18,22 @@ import { DmsModule } from './dms/dms.module';
     WorkspacesModule,
     ChannelsModule,
     DmsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: process.env.DB_USERNAME, // 'root',
+      password: process.env.DB_PASSWORD, // 'root',
+      database: process.env.DB_DATABASE, // 'test',
+      entities: [],
+      // entities: ['entities/*.ts'],
+      // autoLoadEntities: true,
+      synchronize: false,
+      logging: true,
+      keepConnectionAlive: true, // TODO: deprecated 됨
+      charset: 'utf8mb4', // 이모티콘 사용을 위해
+    }),
+    // TypeOrmModule.forFeature([Users]), // autoLoadEntities: true 하면 여기서 설정한 entities 을 읽어온다.
   ],
   controllers: [AppController],
   providers: [AppService, ConfigService],
