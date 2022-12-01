688c7e2440ae2c40669bb65a03648e4bd642b9b4
elle510 <elle0510@gmail.com>
2022-12-01 17:46:04 +0900

    ParseIntPipe 사용 - workspaces

--- back/src/workspaces/workspaces.controller.ts
+++ back/src/workspaces/workspaces.controller.ts
@@ -1,14 +1,38 @@
-import { Controller, Delete, Get, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { User } from 'src/common/decorators/user.decorator';
+import { Users } from 'src/entities/Users';
+import { CreateWorkspaceDto } from './dto/create-workspace.dto';
+import { WorkspacesService } from './workspaces.service';
 
 @ApiTags('WORKSPACE')
 @Controller('api/workspaces')
 export class WorkspacesController {
-  @Get()
-  getMyWorkspaces() {}
+  constructor(private workspacesService: WorkspacesService) {}
 
+  @ApiOperation({ summary: '내 워크스페이스 가져오기' })
+  @Get()
+  async getMyWorkspaces(@User() user: Users) {
+    return this.workspacesService.findMyWorkspaces(user.id);
+  }
+
+  @ApiOperation({ summary: '워크스페이스 만들기' })
   @Post()
-  createWorkspace() {}
+  createWorkspace(@User() user: Users, @Body() body: CreateWorkspaceDto) {
+    return this.workspacesService.createWorkspace(
+      body.workspace,
+      body.url,
+      user.id,
+    );
+  }
 
   @Get(':url/members')
   getAllMembersFromWorkspace() {}
@@ -19,11 +43,20 @@ export class WorkspacesController {
   @Delete(':url/members/:id')
   kickMemberFromWorkspace() {}
 
+  @ApiOperation({ summary: '워크스페이스 특정멤버 가져오기' })
   @Get(':url/members/:id')
-  getMemberInfoInWorkspace() {}
+  async getMemberInfoInWorkspace(
+    @Param('url') url: string,
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    return this.workspacesService.getWorkspaceMember(url, id);
+  }
 
   @Get(':url/users/:id')
-  DEPRECATED_getMemberInfoInWorkspace() {
-    this.getMemberInfoInWorkspace();
+  async DEPRECATED_getMemberInfoInWorkspace(
+    @Param('url') url: string,
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    return this.workspacesService.getWorkspaceMember(url, id);
   }
 }
--- back/src/workspaces/workspaces.module.ts
+++ back/src/workspaces/workspaces.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { WorkspacesService } from './workspaces.service';
 import { WorkspacesController } from './workspaces.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Workspaces } from 'src/entities/Workspaces';
+import { ChannelMembers } from 'src/entities/ChannelMembers';
+import { Channels } from 'src/entities/Channels';
+import { Users } from 'src/entities/Users';
+import { WorkspaceMembers } from 'src/entities/WorkspaceMembers';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      Workspaces,
+      Channels,
+      WorkspaceMembers,
+      ChannelMembers,
+      Users,
+    ]),
+  ],
   providers: [WorkspacesService],
   controllers: [WorkspacesController],
 })
