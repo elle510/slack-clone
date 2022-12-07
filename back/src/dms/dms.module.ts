7ccce0e703bde85dd4df325a74310a3f0cf57705
elle510 <elle0510@gmail.com>
2022-12-07 17:31:36 +0900

    channels/dms typeorm 의존성 주입 누락으로 인해 추가

--- back/src/dms/dms.module.ts
+++ back/src/dms/dms.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DmsService } from './dms.service';
 import { DmsController } from './dms.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DMs } from 'src/entities/DMs';
+import { Users } from 'src/entities/Users';
+import { Workspaces } from 'src/entities/Workspaces';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([DMs, Users, Workspaces])],
   providers: [DmsService],
   controllers: [DmsController],
 })
